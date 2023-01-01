e113edc57b448a1a3db7e20edab9c315a73e6704
Altay Hodoglugil <altay.hodo@gmail.com>
2023-01-01 13:53:56 -0800

    Add 5 games function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let playerChoice = 0;
 let computerChoice = 0;
-
+let playerWins = 0;
+let computerWins = 0;
 
 let addedText = document.createElement('h2')
 let result = document.querySelector('#result')
 let playerResult = document.querySelector('#playerResult')
 let computerResult = document.querySelector('#computerResult')
+let playerDisplay = document.querySelector('#playerDisplay')
+let computerDisplay = document.querySelector('#computerDisplay')
+
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -15,6 +19,22 @@ buttons.forEach((button) => {
         playerResult.textContent = `You Picked: ${playerChoice}`
         computerResult.textContent = `Computer Picked: ${computerChoice}`
         result.textContent = `Result: ${checkWinner()}`
+        playerDisplay.textContent = `Your wins: ${playerWins}`;
+        computerDisplay.textContent = `Computer wins: ${computerWins}`;
+        if(playerWins == 5){
+            alert("You won! Good job :D");
+            playerWins = 0;
+            computerWins = 0;
+            playerDisplay.textContent = `Your wins: ${0}`;
+            computerDisplay.textContent = `Computer wins: ${0}`;
+        }
+        if(computerWins == 5){
+            alert("You lost loser lmao");
+            playerWins = 0;
+            computerWins = 0;
+            playerDisplay.textContent = `Your wins: ${0}`;
+            computerDisplay.textContent = `Computer wins: ${0}`;
+        }
     })
 })
 
@@ -42,9 +62,11 @@ function checkWinner(){
         (playerChoice == 'Paper' && computerChoice == 'Rock') ||
         (playerChoice == 'Scissors' && computerChoice == 'Paper')
         ){
+            playerWins++;
             return 'You Win';
         }
     else{
+        computerWins++;
         return 'Computer Wins';
     }
 }
